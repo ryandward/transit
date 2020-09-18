babc7b5a7c7fd9b978e1f05d1e98c68725d704f4
ryandward <ryandward@gmail.com>
2020-09-18 01:12:42 -0500

    Added an awk script to transform tbl files from NCBI into the more useable bed file

--- README.sh
+++ README.sh
@@ -2,7 +2,7 @@
 
 GENOME_TARGET="U00096.3"
 
-conda create -n transit -c bioconda 'bowtie=1.2.3' entrez-direct git transit 'python>3'
+#conda create -n transit -c bioconda 'bowtie=1.2.3' entrez-direct git transit samtools bedtools 'python>3'
 
 conda activate transit
 
@@ -18,11 +18,11 @@ efetch -db nuccore -format ft -id $GENOME_TARGET > ${GENOME_TARGET}.tbl &&
       echo "${GENOME_TARGET}.tbl contains data, continue to next step." ||
       echo "Emtpy file: ${GENOME_TARGET}.tbl, try efetch step again."
 
-bowtie-build -o2 ${GENOME_TARGET}.fasta $GENOME_TARGET
+bowtie-build -o1 ${GENOME_TARGET}.fasta $GENOME_TARGET
 
 bowtie -S -m2 --threads 12 --no-unal --best --strata --tryhard \
-$GENOME_TARGET <(cat ../Reads/ERR2249109.fastq ../Reads/ERR2249110.fastq) |
-samtools view -S -b > ${GENOME_TARGET}_aln.bam
+$GENOME_TARGET <(awk 'BEGIN {FS = "\t" ; OFS = "\n"} {header = $0 ; getline seq ; getline qheader ; getline qseq ; if (length(seq) >= 25) {print header, seq, qheader, qseq}}' ${GENOME_TARGET}_Reads/*.fastq) |
+samtools view -S -b -F 4 > ${GENOME_TARGET}_aln.bam
 
 samtools sort ${GENOME_TARGET}_aln.bam > ${GENOME_TARGET}_aln.bam.sort
 
@@ -30,9 +30,10 @@ mv ${GENOME_TARGET}_aln.bam.sort ${GENOME_TARGET}_aln.bam
 
 bam2wig.pl  --in ${GENOME_TARGET}_aln.bam --start --rpm --out ${GENOME_TARGET}_aln.wig
 
-~/SamsungUSB/Scripts/tbl2bed.awk ${GENOME_TARGET}.tbl |
-awk '$7=="CDS" {print $1,$2,$3,$6,($3-$2)/3,$7,$8,$5,$4}' > ${GENOME_TARGET}_aln.wig
+./tbl2bed.awk ${GENOME_TARGET}.tbl |
+awk '$7=="CDS" {print $1,$2,$3,$6,($3-$2)/3,$7,$8,$5,$4}' > ${GENOME_TARGET}.prot_table
+
 
 transit tn5gaps -m2 -r mean ${GENOME_TARGET}_aln.wig ${GENOME_TARGET}.prot_table ${GENOME_TARGET}_transit_tn5gaps.tsv
 
-conda deactivate
+# conda deactivate
---
+++ tbl2bed.awk
@@ -0,0 +1,91 @@
+#!/usr/bin/gawk -E
+#July 4, 2020 - Ryan D. Ward
+
+BEGIN{
+  FS="\t";
+  OFS="\t";
+  PROCINFO["sorted_in"] = "@ind_num_asc";
+}
+
+($3 || $0~"^>") && n && locus_tag{
+  for(i = 1; i <= n; i++){
+    print chr,
+    left[feature][i],
+    right[feature][i],
+    locus_tag,
+    gene,
+    strand[feature][i],
+    pseudo""feature,
+    complete
+  }
+  n=0
+}
+
+
+$0~"^>"{
+
+  f1 = match($0,/\|(.*?)\|/,chr_match);
+  if(f1){chr=chr_match[1]; next}
+  f2 = match($0,/ (.*?)/,chr_match);
+  if(f2){chr=chr_match[1];}
+
+  next;
+}
+
+$3 && ($3 != "CDS" && $3 != "tRNA" && $3 != "ncRNA" && $3 != "rRNA") {
+  independent_feature_count++;
+  gene = ".";
+  this_file = FILENAME;
+  split(this_file, file_parts, ".");
+  this_file = file_parts[1];
+  locus_tag = this_file"_"independent_feature_count;
+  split(locus_tag,locus_tag_parts,"/")
+  locus_tag = locus_tag_parts[length(locus_tag_parts)]
+  delete(left);
+  delete(right);
+}
+
+$3 {
+  pseudo = "";
+  complete = "";
+  feature = $3;
+}
+
+$1 && $2 {
+  n++;
+
+  n1 = gsub(/[^0-9]/, "", $1);
+  n2 = gsub(/[^0-9]/, "", $2);
+
+  left[feature][n] = $2 > $1 ? $1-1 : $2-1
+  right[feature][n] = $2 > $1 ? $2 : $1
+  strand[feature][n] = $2 > $1 ? "+" : "-"
+
+  complete = n1+n2 > 0 ? "partial" : "complete"
+}
+
+$4 == "pseudo" {
+  pseudo = "pseudo-"
+}
+
+$4 == "locus_tag" {
+  locus_tag = $5;
+}
+
+$4 == "gene" {
+  gene = $5
+}
+
+
+ END{
+     for(i = 1; i <= n; i++){
+       print chr,
+         left[feature][i],
+         right[feature][i],
+         locus_tag,
+         gene,
+         strand[feature][i],
+         pseudo""feature,
+         complete     }
+     n=0
+ }
